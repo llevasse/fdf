c7d7ad5cefa15b3b29e606e6e7a243148d3389a3
Leo <leonardlevasseur@yahoo.com>
2023-02-11 23:40:01 +0100

    first pixel

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 11:58:01 by llevasse          #+#    #+#             */
-/*   Updated: 2023/02/10 16:38:01 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/02/11 23:21:08 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,28 @@
 # include "../libft/libft.h"
 # include <fcntl.h>
 # include <mlx.h>
+# include <X11/keysym.h>
+
+# define WINDOW_WIDTH 1220
+# define WINDOW_HEIGHT 1080
+
+# define WHITE 0xffffff
+
+typedef struct s_img
+{
+	void	*mlx_img;
+	char	*addr;
+	int		bpp;
+	int		line_len;
+	int		endian;
+}	t_img;
+
+typedef struct s_data
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	t_img	img;
+}	t_data;
+
 
 #endif
--- srcs/fdf.c
+++ srcs/fdf.c
@@ -6,21 +6,74 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 11:55:30 by llevasse          #+#    #+#             */
-/*   Updated: 2023/02/10 16:50:14 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/02/11 23:39:32 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
 
-int	main(int argc, char *argv[])
+int	handle_no_event(void *data)
 {
-	void	*mlx_ptr;
-
-	mlx_ptr = mlx_init();
- 	mlx_destroy_display(mlx_ptr);
-	free(mlx_ptr);
-	(void)mlx_ptr;
-	(void)argc;
-	(void)argv;
+	(void)data;
 	return (0);
+}
+
+int	handle_input(int keysym, t_data *data)
+{
+	if (keysym == XK_Escape)
+	{
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+		data->win_ptr = NULL;
+	}
+	return (0);
+}
+
+void	img_pix_put(t_img *img, int x, int y, int color)
+{
+	char	*pixel;
+	int		i;
+
+	i = img->bpp - 8;
+	pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
+	while (i >= 0)
+	{
+		if (img->endian != 0)
+			*pixel++ = (color >> i) & 0xFF;
+		else
+			*pixel++ = (color >> (img->bpp - 8 - i)) & 0xFF;
+		i -= 8;
+	}
+}
+
+int	render(t_data *data)
+{
+	if (!data->win_ptr)
+		return (1);
+	img_pix_put(&data->img, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, WHITE);
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
+		data->img.mlx_img, 0, 0);
+	return (0);
+}
+
+int	main(void)
+{
+	t_data	data;
+
+	data.mlx_ptr = mlx_init();
+	if (!data.mlx_ptr)
+		return (1);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT,
+			"fdf");
+	if (!data.win_ptr)
+		return (free(data.win_ptr), 1);
+	data.img.mlx_img = mlx_new_image(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
+	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp,
+			&data.img.line_len, &data.img.endian);
+	mlx_loop_hook(data.mlx_ptr, &render, &data);
+	mlx_key_hook(data.win_ptr, &handle_input, &data);
+
+	mlx_loop(data.mlx_ptr);
+
+	mlx_destroy_display(data.mlx_ptr);
+	free(data.mlx_ptr);
 }
