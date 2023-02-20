b731dc0ca9eac54c61ce0db8259df9d3822e0c7a
Leo <leonardlevasseur@yahoo.com>
2023-02-21 00:33:32 +0100

    better gradiant if pointa is higher than point b and point a is lower than  data.highest_altitude

--- srcs/colour.c
+++ srcs/colour.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 13:40:33 by llevasse          #+#    #+#             */
-/*   Updated: 2023/02/20 13:10:32 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/02/21 00:30:48 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,10 +75,16 @@ unsigned int	get_rgb(t_line line, int i, t_data data)
 	if (line.z_ratio == 0 && line.altitude_a == data.lowest_altitude)
 		return ((LOWEST_R * 256 * 256) + (LOWEST_G * 256) + LOWEST_B);
 	len = get_len_with_high_altitude(line, data);
+	if (line.altitude_a && line.altitude_a < line.altitude_b)
+		i += len / 2;
+	if (line.altitude_b && line.altitude_b < line.altitude_a)
+		i += len / 2;
 	if (line.altitude_a < line.altitude_b)
 		percent_gradiant = (double)i / len;
 	else
 		percent_gradiant = (double)(len - i) / len;
+	if (line.z_ratio > 0 && line.altitude_a > line.altitude_b)
+		percent_gradiant -= (double)line.altitude_a / data.highest_altitude;
 	if (percent_gradiant > 1.0)
 		percent_gradiant = 1;
 	if (line.z_ratio > 0)
@@ -105,7 +111,9 @@ int	get_len_with_high_altitude(t_line line, t_data data)
 
 	y_a = line.y_a;
 	if (line.altitude_a != 0)
-		y_a *= data.highest_altitude / line.altitude_a;
+		y_a *= line.altitude_a / data.highest_altitude;
+	if (line.altitude_a != 0 && line.altitude_b > line.altitude_a)
+		y_a = line.y_a + line.altitude_a;
 	y_b = line.y_b;
 	if (line.altitude_b != 0)
 		y_b *= data.highest_altitude / line.altitude_b;
