d6b9a9125a64f1792f5c322cbc78d29c6db95f3e
Leo <leonardlevasseur@yahoo.com>
2023-02-22 20:57:43 +0100

    less confusing use of move

--- srcs/move.c
+++ srcs/move.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 16:49:07 by llevasse          #+#    #+#             */
-/*   Updated: 2023/02/21 17:09:27 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/02/22 20:57:23 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	move_left(t_data *data, int value)
 	reset_img(data);
 	clear_point(*data);
 	clear_line(*data);
-	data->grid.x_decal -= value;
+	data->grid.x_decal += value;
 	data->points = parse_points(*data);
 	data->lines = get_all_lines(*data);
 	render(data);
@@ -28,7 +28,7 @@ void	move_right(t_data * data, int value)
 	reset_img(data);
 	clear_point(*data);
 	clear_line(*data);
-	data->grid.x_decal += value;
+	data->grid.x_decal -= value;
 	data->points = parse_points(*data);
 	data->lines = get_all_lines(*data);
 	render(data);
@@ -39,7 +39,7 @@ void	move_up(t_data *data, int value)
 	reset_img(data);
 	clear_point(*data);
 	clear_line(*data);
-	data->grid.y_decal -= value;
+	data->grid.y_decal += value;
 	data->points = parse_points(*data);
 	data->lines = get_all_lines(*data);
 	render(data);
@@ -50,7 +50,7 @@ void	move_down(t_data * data, int value)
 	reset_img(data);
 	clear_point(*data);
 	clear_line(*data);
-	data->grid.y_decal += value;
+	data->grid.y_decal -= value;
 	data->points = parse_points(*data);
 	data->lines = get_all_lines(*data);
 	render(data);
