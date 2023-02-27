7880dc68936697b9312d96437224ed713a381d83
Leo <leonardlevasseur@yahoo.com>
2023-02-27 13:38:35 +0100

    added function to reset ptr of lines and points

--- srcs/clear.c
+++ srcs/clear.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 15:35:58 by llevasse          #+#    #+#             */
-/*   Updated: 2023/02/26 13:03:57 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/02/27 13:37:59 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,18 +27,33 @@ void	clear_split(char **str)
 	str = NULL;
 }
 
-void	clear_point(t_data data)
+void	reset_ptr_point(t_data data)
 {
- 	int	x;
-	int	y;
-	
-	y = 0;
-	x = 0;
+	int	i;
+
+	i = 0;
 	if (data.points[0][0] == NULL)
 	{
-		while (x < data.nb_line)
-			data.points[x++] -= data.elem_per_line;
+		while (i < data.nb_line)
+			data.points[i++] -= data.elem_per_line;
 	}
+}
+
+void	reset_ptr_lines(t_data data)
+{
+	if (!(*data.lines))
+		data.lines--;
+	while ((*data.lines)->line_id != 0 || !(*data.lines))
+		data.lines--;
+}
+
+void	clear_point(t_data data)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	reset_ptr_point(data);
 	while (data.points[y])
 	{
 		x = 0;
@@ -51,31 +66,20 @@ void	clear_point(t_data data)
 		free(data.points[y]);
 		data.points[y++] = NULL;
 	}
-	free (data.points[y]);
-	free (data.points);
+	free(data.points[y]);
+	free(data.points);
 	data.points = NULL;
 }
 
 void	clear_line(t_data data)
 {
-	int	i;
-
-	i = 1;
-	if (!(*data.lines))
-		data.lines--;
-	while ((*data.lines)->line_id != 0 || !(*data.lines))
-	{
-		data.lines--;
-		i++;
-	}
 	while (*data.lines)
 	{
 		free(*data.lines);
 		*(data.lines)++ = NULL;
 	}
-	free (*data.lines);
-	data.lines -= i;
-	free (data.lines);
+	free(*data.lines);
+	free(data.lines);
 	data.lines = NULL;
 	(void)data;
 }
