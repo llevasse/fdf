a2f7c480bc10f6dc57f944efdb62e8219ce8b13c
Leo <leonardlevasseur@yahoo.com>
2023-02-28 11:10:31 +0100

    zoom more smoothly

--- srcs/zoom.c
+++ srcs/zoom.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 00:47:41 by llevasse          #+#    #+#             */
-/*   Updated: 2023/02/24 10:02:16 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/02/28 11:09:42 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,27 @@
 void	zoom_in(t_data *data)
 {
 	reset_img(data);
-	clear_point(*data);
-	clear_line(*data);
 	data->grid.zoom *= 1.5;
-	data->points = parse_points(*data);
+	set_point(data);
 	set_colour(data);
-	data->lines = get_all_lines(*data);
+	set_lines(data);
 	render(data);
 }
 
 void	zoom_out(t_data *data)
 {
 	reset_img(data);
-	clear_point(*data);
-	clear_line(*data);
 	data->grid.zoom *= 0.5;
-	data->points = parse_points(*data);
+	set_point(data);
 	set_colour(data);
-	data->lines = get_all_lines(*data);
-	render(data);		
+	set_lines(data);
+	render(data);
 }
 
 void	reset_img(t_data *data)
 {
-	int	x;
-	int	y;
+	int x;
+	int y;
 
 	y = 0;
 	if (!data->win_ptr)
@@ -51,6 +47,6 @@ void	reset_img(t_data *data)
 			img_pix_put(&data->img, x++, y, BG);
 		y++;
 	}
-	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
-	data->img.mlx_img, 0, 0);
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.mlx_img, 0,
+			0);
 }
